a04ebe458fee1c2ed50bab5dcf5f9655b6f76117
victor <viih.higa@gmail.com>
2018-03-19 22:41:08 -0300

    aula2

---
+++ arqsw_sdesk_a2/src/br/usjt/arqsw/service/FilaService.java
@@ -0,0 +1,24 @@
+package br.usjt.arqsw.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.usjt.arqsw.dao.FilaDAO;
+import br.usjt.arqsw.entity.Fila;
+
+public class FilaService {
+	private FilaDAO dao;
+	
+	@Autowired
+	public FilaService(FilaDAO dao) {
+		this.dao = dao;
+	}
+	public ArrayList<Fila> listarFilas() throws IOException{
+		return dao.listarFilas();
+	}
+	public Fila carregar(int id) throws IOException{
+		return dao.carregar(id);
+	}
+}
